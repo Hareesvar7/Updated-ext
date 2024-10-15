da27933eb5c75af32c8d352196fbeb5c381ac27a
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-15 21:55:29 +0530

    Update Storageservice.js

--- Storageservice.js
+++ Storageservice.js
@@ -1,27 +1,32 @@
-// StorageService.js
 const vscode = require('vscode');
 const { getTemplate } = require('./StorageTemplateService');
 
 function activate(context) {
     let disposable = vscode.commands.registerCommand('extension.insertTemplate', async () => {
         const editor = vscode.window.activeTextEditor;
+
         if (editor) {
             const document = editor.document;
             const position = editor.selection.active;
-
-            // Get the text in the document
             const text = document.getText();
+            const keywords = ['aws_s3_template', 'azure_storage_template', 'gcp_storage_template'];
+            let templateFound = false;
 
-            // Check for specific keywords
-            if (text.includes('aws_s3_template')) {
-                const template = getTemplate('aws_s3_template');
-                if (template) {
-                    await editor.edit(editBuilder => {
-                        editBuilder.insert(position, template);
-                    });
-                } else {
-                    vscode.window.showWarningMessage('Template not found');
+            for (const keyword of keywords) {
+                if (text.includes(keyword)) {
+                    const template = getTemplate(keyword);
+                    if (template) {
+                        await editor.edit(editBuilder => {
+                            editBuilder.insert(position, template);
+                        });
+                        templateFound = true;
+                        break;
+                    }
                 }
+            }
+
+            if (!templateFound) {
+                vscode.window.showWarningMessage('Template not found');
             }
         }
     });
