f53e28b47f124805c593e41b9cebe4c55c06e3bf
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-15 22:05:51 +0530

    Update Storageservice.js

--- Storageservice.js
+++ Storageservice.js
@@ -1,39 +1,15 @@
 const vscode = require('vscode');
-const { getTemplate } = require('./StorageTemplateService');
 
-function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.insertTemplate', async () => {
-        const editor = vscode.window.activeTextEditor;
+class StorageService {
+    constructor() {
+        this.keywords = ['aws_s3_template', 'azure_storage_template', 'gcp_storage_template'];
+    }
 
-        if (editor) {
-            const document = editor.document;
-            const position = editor.selection.active;
-            const text = document.getText();
-            const keywords = ['aws_s3_template', 'azure_storage_template', 'gcp_storage_template'];
-            let templateFound = false;
-
-            for (const keyword of keywords) {
-                if (text.includes(keyword)) {
-                    const template = getTemplate(keyword);
-                    if (template) {
-                        await editor.edit(editBuilder => {
-                            editBuilder.insert(position, template);
-                        });
-                        templateFound = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!templateFound) {
-                vscode.window.showWarningMessage('Template not found');
-            }
-        }
-    });
-
-    context.subscriptions.push(disposable);
+    isValidKeyword(keyword) {
+        return this.keywords.includes(keyword);
+    }
 }
 
-module.exports = {
-    activate,
-};
+// Exporting an instance of StorageService
+const storageService = new StorageService();
+module.exports = storageService;
