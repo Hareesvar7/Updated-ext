0aa820769782f220a245c4fa82a53e880495a29a
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-15 22:16:34 +0530

    Update Extensionwithtemplate.js

--- Extensionwithtemplate.js
+++ Extensionwithtemplate.js
@@ -1,37 +1,10 @@
 const vscode = require('vscode');
-const getTemplate = require('./StorageTemplateService');
-const StorageService = require('./StorageService');
+const storageTemplateCommand = require('./commands/StorageTemplate');
 
 function activate(context) {
-    const storageService = new StorageService();
-
     let disposable = vscode.commands.registerCommand('extension.insertTemplate', async () => {
         const editor = vscode.window.activeTextEditor;
-
-        if (editor) {
-            const document = editor.document;
-            const position = editor.selection.active;
-            const text = document.getText();
-            const keywords = storageService.keywords;
-            let templateFound = false;
-
-            for (const keyword of keywords) {
-                if (text.includes(keyword)) {
-                    const template = getTemplate(keyword);
-                    if (template) {
-                        await editor.edit(editBuilder => {
-                            editBuilder.insert(position, template);
-                        });
-                        templateFound = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!templateFound) {
-                vscode.window.showWarningMessage('Template not found');
-            }
-        }
+        await storageTemplateCommand.insertTemplate(editor);
     });
 
     context.subscriptions.push(disposable);
