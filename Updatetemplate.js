530b823c869c33db1740c90836b36ac6115f96a5
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-15 23:23:12 +0530

    Create Updatetemplate.js

---
+++ Updatetemplate.js
@@ -0,0 +1,51 @@
+class StorageTemplateService {
+    constructor() {
+        this.templates = {
+            aws_s3_template: this.getAwsS3Template(),
+            azure_storage_template: this.getAzureStorageTemplate(),
+            gcp_storage_template: this.getGcpStorageTemplate(),
+        };
+    }
+
+    getAwsS3Template() {
+        return `package aws.s3.policies
+
+deny[msg] {
+    resource := input.resource_changes[_]
+    resource.type == "aws_s3_bucket"
+    not resource.change.after.server_side_encryption.enabled
+    msg = sprintf("S3 bucket '%s' must have server-side encryption enabled", [resource.change.after.bucket])
+}
+`;
+    }
+
+    getAzureStorageTemplate() {
+        return `package azure.storage.policies
+
+deny[msg] {
+    resource := input.resource_changes[_]
+    resource.type == "azurerm_storage_account"
+    not resource.change.after.enable_https_traffic_only
+    msg = sprintf("Azure Storage Account '%s' must enforce HTTPS traffic only", [resource.change.after.name])
+}
+`;
+    }
+
+    getGcpStorageTemplate() {
+        return `package gcp.storage.policies
+
+deny[msg] {
+    resource := input.resource_changes[_]
+    resource.type == "google_storage_bucket"
+    not resource.change.after.default_event_based_hold
+    msg = sprintf("GCP Storage Bucket '%s' must have event-based hold enabled", [resource.change.after.name])
+}
+`;
+    }
+
+    getTemplate(keyword) {
+        return this.templates[keyword] || null;
+    }
+}
+
+module.exports = new StorageTemplateService();
